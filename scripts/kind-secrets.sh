ed403e4973bcd8dbebf0034342efa6c1ac9f04cd
Malcolm Jones <bossjones@theblacktonystark.com>
2024-03-22 13:43:54 -0400

    chg: secrets

--- scripts/kind-secrets.sh
+++ scripts/kind-secrets.sh
@@ -9,6 +9,7 @@ echo "# parent path -------------->  ${0%/*}  "
 echo "# my name ------------------>  ${0##*/} "
 echo ""
 
+export GIT_URI=$(git config --get remote.origin.url | sed -e 's/:/\//g' | sed -e 's/ssh\/\/\///g' | sed -e 's/git@/https:\/\//g' | sed 's/.git$//')
 
 cat <<EOF | kubectl apply -f -
 apiVersion: v1
@@ -19,5 +20,37 @@ metadata:
 type: Opaque
 stringData:
     key.txt: |
-        $(cat $HOME/.sops/key.txt | base64)
+        $(cat "$HOME"/.sops/key.txt | base64)
+EOF
+
+
+
+# # set -euxo pipefail
+# kubectl apply -f - <<EOF
+# apiVersion: v1
+# kind: Secret
+# metadata:
+#   name: main-repository
+#   namespace: argocd
+#   labels:
+#     argocd.argoproj.io/secret-type: repository
+# stringData:
+#   type: git
+#   url: ${GIT_URI}
+#   password: ${GH_PASS}
+#   username: ${GH_USER}
+# EOF
+
+kubectl apply -f - <<EOF
+apiVersion: v1
+kind: Secret
+metadata:
+  name: private-repo-creds
+  labels:
+    argocd.argoproj.io/secret-type: repo-creds
+stringData:
+  type: git
+  url: ${GIT_URI}
+  password: ${GH_PASS}
+  username: ${GH_USER}
 EOF
